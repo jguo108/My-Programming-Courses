f0efd3a4600882f1629efb7a434265cf97b42d95
jguo108 <chairmanguo@hotmail.com>
2024-06-19 16:48:48 +0800

    update

---
+++ Python/Level0/Tkinter Library/egg_catcher.py
@@ -0,0 +1,165 @@
+﻿# Coding Projects in Python
+from itertools import cycle
+from random import randrange
+import tkinter as tk
+from tkinter import messagebox, font
+
+
+def setup_scene():
+    # Grass
+    canvas.create_rectangle(-5, canvas_height-100, canvas_width+5,
+                            canvas_height+5, fill='sea green', width=0)
+    # Sun
+    canvas.create_oval(-80, -80, 120, 120, fill='orange', width=0)
+
+
+def create_catcher():
+    return canvas.create_arc(
+        catcher_start_x,
+        catcher_start_y,
+        catcher_start_x2,
+        catcher_start_y2,
+        start=200,
+        extent=140,
+        style='arc',
+        outline=catcher_color,
+        width=3)
+
+
+def create_score():
+    return canvas.create_text(
+        10, 10, anchor='nw', text='Score: ' + str(score), fill='darkblue', font=game_font)
+
+
+def create_lives():
+    return canvas.create_text(
+        canvas_width-10, 10, anchor='ne', text='Lives: ' + str(lives_remaining), fill='darkblue', font=game_font
+    )
+
+
+def create_eggs():
+    x = randrange(10, 740)
+    y = 40
+    new_egg = canvas.create_oval(
+        x, y, x+egg_width, y+egg_height, fill=next(egg_colors), width=0)
+    eggs.append(new_egg)
+    window.after(egg_interval, create_eggs)
+
+
+def lose_a_life():
+    global lives_remaining
+    lives_remaining -= 1
+    canvas.itemconfigure(lives_text, text='Lives: ' + str(lives_remaining))
+
+
+def egg_dropped(egg):
+    eggs.remove(egg)
+    canvas.delete(egg)
+    lose_a_life()
+    if lives_remaining == 0:
+        messagebox.showinfo('Game Over!', 'Final Score: ' + str(score))
+        window.destroy()
+
+
+def move_eggs():
+    for egg in eggs:
+        (egg_x, egg_y, egg_x2, egg_y2) = canvas.coords(egg)
+        canvas.move(egg, 0, 10)
+        if egg_y2 > canvas_height:
+            egg_dropped(egg)
+    window.after(egg_speed, move_eggs)
+
+
+def increase_score(points):
+    global score, egg_speed, egg_interval
+    score += points
+    # increase speed by difficulty_factor
+    egg_speed = int(egg_speed * difficulty_factor)
+    egg_interval = int(egg_interval * difficulty_factor)
+    canvas.itemconfigure(score_text, text='Score: ' + str(score))
+
+
+def check_catch():
+    (catcher_x, catcher_y, catcher_x2, catcher_y2) = canvas.coords(catcher)
+    for egg in eggs:
+        (egg_x, egg_y, egg_x2, egg_y2) = canvas.coords(egg)
+        if catcher_x < egg_x and egg_x2 < catcher_x2 and catcher_y2 - egg_y2 < 40:
+            eggs.remove(egg)
+            canvas.delete(egg)
+            increase_score(egg_score)
+    window.after(100, check_catch)
+
+
+def move_left(event):
+    (x1, y1, x2, y2) = canvas.coords(catcher)
+    if x1 > 0:
+        canvas.move(catcher, -20, 0)
+
+
+def move_right(event):
+    (x1, y1, x2, y2) = canvas.coords(catcher)
+    if x2 < canvas_width:
+        canvas.move(catcher, 20, 0)
+
+
+canvas_width = 800
+canvas_height = 600
+
+egg_colors = cycle(
+    ['light blue', 'light green', 'light pink', 'light yellow', 'light cyan'])
+
+# egg variables
+egg_width = 45
+egg_height = 55
+egg_score = 10
+egg_speed = 500
+egg_interval = 4000
+difficulty_factor = 0.95
+
+# catcher variables
+catcher_color = 'blue'
+catcher_width = 100
+catcher_height = 100
+catcher_start_x = canvas_width/2-catcher_width/2
+catcher_start_y = canvas_height-catcher_height-20
+catcher_start_x2 = catcher_start_x+catcher_width
+catcher_start_y2 = catcher_start_y+catcher_height
+
+# lives
+lives_remaining = 3
+
+# score
+score = 0
+
+# eggs
+eggs = []
+
+window = tk.Tk()
+window.resizable(False, False)
+canvas = tk.Canvas(
+    window,
+    width=canvas_width,
+    height=canvas_height,
+    background='deep sky blue')
+canvas.pack()
+
+# font
+game_font = font.nametofont('TkFixedFont')
+game_font.config(size=18)
+
+setup_scene()
+catcher = create_catcher()
+score_text = create_score()
+lives_text = create_lives()
+
+# Keyboard movement
+canvas.bind('<Left>', move_left)
+canvas.bind('<Right>', move_right)
+canvas.focus_set()
+
+# After one second start eggs, then move, then check for catch
+window.after(1000, create_eggs)
+window.after(1000, move_eggs)
+window.after(1000, check_catch)
+
+window.mainloop()
---
+++ Python/Level0/Tkinter Library/snake_game.py
@@ -0,0 +1,149 @@
+﻿# https://www.youtube.com/watch?v=FtqWCo1_I4g
+
+import tkinter as tk
+import random
+
+ROWS = 25
+COLS = 25
+TILE_SIZE = 25
+WINDOW_WIDTH = COLS * TILE_SIZE
+WINDOW_HEIGHT = ROWS * TILE_SIZE
+
+
+class Tile:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+window = tk.Tk()
+window.title('Snake Game')
+window.resizable(False, False)  # Disable resizing
+
+# center the window (optional)
+'''
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+window.geometry(f'{window_width}x{window_height}+{x}+{y}')
+'''
+
+# Create canvas
+canvas = tk.Canvas(master=window, bg='black',
+                   width=WINDOW_WIDTH,
+                   height=WINDOW_HEIGHT,
+                   borderwidth=0,  # Remove border
+                   highlightthickness=0)  # Remove highlight
+canvas.pack()
+# window.update()
+
+
+snake = Tile(5*TILE_SIZE, 5*TILE_SIZE)  # Single tile, snakes's head
+snake_body = []  # mutilpe Tile objects
+direction_x = 0
+direction_y = 0
+food = Tile(10*TILE_SIZE, 10*TILE_SIZE)  # Food
+game_over = False
+score = 0
+
+
+def change_direction(e):
+    global direction_x, direction_y
+
+    if game_over:
+        return
+
+    if e.keysym == 'Up' and direction_y != 1:  # when the snake is going up we cannot make it go down
+        direction_x = 0
+        direction_y = -1
+    elif e.keysym == 'Down' and direction_y != -1:  # when the snake is going down we cannot make it go up
+        direction_x = 0
+        direction_y = 1
+    elif e.keysym == 'Left' and direction_x != 1:  # when the snake is going left we cannot make it go right
+        direction_x = -1
+        direction_y = 0
+    elif e.keysym == 'Right' and direction_x != -1:  # when the snake is going right we cannot make it go left
+        direction_x = 1
+        direction_y = 0
+
+
+def move():
+    global snake, game_over, food, snake_body, score
+    if game_over:
+        return
+
+    # if snake touches the edge
+    if snake.x < 0 or snake.x >= WINDOW_WIDTH or snake.y < 0 or snake.y >= WINDOW_HEIGHT:
+        game_over = True
+        return
+
+    # if snake touches itself
+    for tile in snake_body:
+        if tile.x == snake.x and tile.y == snake.y:
+            game_over = True
+            return
+
+    # check for collision with food
+    if snake.x == food.x and snake.y == food.y:
+        snake_body.append(Tile(food.x, food.y))
+        food.x = random.randint(0, COLS-1) * TILE_SIZE
+        food.y = random.randint(0, ROWS-1) * TILE_SIZE
+        score += 1
+
+    # update snake body
+    for i in range(len(snake_body)-1, -1, -1):
+        tile = snake_body[i]
+        if i == 0:
+            tile.x = snake.x
+            tile.y = snake.y
+        else:
+            tile.x = snake_body[i-1].x
+            tile.y = snake_body[i-1].y
+
+    snake.x += direction_x * TILE_SIZE
+    snake.y += direction_y * TILE_SIZE
+
+
+def render():
+    global snake, food, snake_body, game_over, score
+    canvas.delete('all')
+
+    # Render food
+    canvas.create_rectangle(food.x, food.y, food.x +
+                            TILE_SIZE, food.y + TILE_SIZE, fill='red')
+
+    # Render snake head
+    canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE,
+                            snake.y + TILE_SIZE, fill='lime green')
+
+    # Render snake body
+    for tile in snake_body:
+        canvas.create_rectangle(tile.x, tile.y, tile.x + TILE_SIZE,
+                                tile.y + TILE_SIZE, fill='lime green')
+
+    if game_over:
+        # Render 'Game over: Score'
+        canvas.create_text(WINDOW_WIDTH/2, WINDOW_HEIGHT/2,
+                           font='Arial 20', text=f'Game Over: {score}', fill='white')
+    else:
+        # Render 'Score'
+        canvas.create_text(30, 20, font='Arial 10',
+                           text=f'Score: {score}', fill='white')
+
+    window.after(100, tick)  # draw rectangle every 100ms, 10FPS
+
+
+def tick():
+    move()
+    render()
+
+
+tick()
+
+window.bind('<KeyRelease>', change_direction)
+
+window.mainloop()
