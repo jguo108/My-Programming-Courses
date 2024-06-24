19f7596058a20ed6c2a63833016e8391a298cbe9
jguo108 <chairmanguo@hotmail.com>
2024-06-24 16:51:01 +0800

    update

--- Python/Level0/Turtle Library/1_modern_art.py
+++ Python/Level0/Turtle Library/1_modern_art.py
@@ -3,27 +3,31 @@
 import turtle
 from random import randint
 
-# Fullscreen the canvas
-screen = turtle.Screen()
-screen.setup(480, 360)  # width and height of the screen
+# Fullwindow the canvas
+window = turtle.Screen()
+window.title('Modern Art')
+window.setup(600, 600)  # width and height of the window
+window.bgcolor('gray10')
 
 # Begin!
 t = turtle.Turtle()  # create a Turtle and it always start at (0, 0)
+t.shape("turtle")
+t.speed(0)
 
 
 def randomcolour():
-    ### Uncomment the line below if you are not using trinket###
     turtle.colormode(255)
     red = randint(0, 255)
     green = randint(0, 255)
     blue = randint(0, 255)
     t.pencolor(red, green, blue)
+    t.fillcolor(red, green, blue)
 
 
 def randomplace():
     t.penup()
-    x = randint(-200, 120)
-    y = randint(-200, 120)
+    x = randint(-200, 200)
+    y = randint(-200, 200)
     t.goto(x, y)
     t.pendown()
 
@@ -34,38 +38,59 @@ def randomheading():
 
 
 def drawrectangle():
-    randomcolour()
-    randomplace()
     t.hideturtle()
     length = randint(10, 150)
     height = randint(10, 150)
+
     t.begin_fill()
-    t.forward(length)
-    t.right(90)
-    t.forward(height)
-    t.right(90)
-    t.forward(length)
-    t.right(90)
-    t.forward(height)
-    t.right(90)
+    for _ in range(2):
+        t.forward(length)
+        t.right(90)
+        t.forward(height)
+        t.right(90)
     t.end_fill()
 
 
-t.shape("turtle")
-t.speed(0)
+def drawsquare():
+    t.hideturtle()
+    side_length = randint(10, 150)
 
-randomcolour()
-randomplace()
-randomheading()
-drawrectangle()
-"""
-for _ in range(1, 30):
-  randomcolour()
-  randomplace()
-  randomheading()
-  drawrectangle()
-  #t.stamp()
+    t.begin_fill()
+    for _ in range(4):
+        t.forward(side_length)
+        t.right(90)
+    t.end_fill()
 
-"""
 
-screen.mainloop()
+def drawcircle():
+    t.hideturtle()
+    radius = randint(10, 100)
+
+    t.begin_fill()
+    t.circle(radius)
+    t.end_fill()
+
+
+def drawstar():
+    t.hideturtle()
+    size = randint(50, 150)
+    num_points = randint(2, 5) * 2 + 1
+    angle = 180 - (180/num_points)
+
+    t.begin_fill()
+    for _ in range(num_points):
+        t.forward(size)
+        t.right(angle)
+    t.end_fill()
+
+
+for i in range(50):
+    randomcolour()
+    randomplace()
+    randomheading()
+    # drawrectangle()
+    # drawsquare()
+    # drawcircle()
+    drawstar()
+
+window.mainloop()
