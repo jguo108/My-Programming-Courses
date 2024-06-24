dc171d6abaf2f466280bfc83e400111d27f08496
jguo108 <chairmanguo@hotmail.com>
2024-06-24 18:38:06 +0800

    Update 2_make_a_face.py

--- Python/Level0/Turtle Library/2_make_a_face.py
+++ Python/Level0/Turtle Library/2_make_a_face.py
@@ -1,4 +1,6 @@
-﻿# This should be a project that shows how you can create
+﻿from random import randint
+import turtle
+# This should be a project that shows how you can create
 # some animation by switching costumes in Python
 
 # https://scratch.mit.edu/projects/237521692/
@@ -9,8 +11,6 @@
 # - reference for costumes:https://www.freepik.com/free-vector/collection-cute-halloween-ghosts_1319212.htm#fromView=search&page=1&position=29&uuid=cf5b65a7-71d0-4fe3-982e-44272b07ac6e
 # - reference for animation: https://scratch.mit.edu/projects/237521692/
 
-import turtle
-from random import randint
 
 # Fullwindow the canvas
 window = turtle.Screen()
@@ -18,38 +18,88 @@ window.title('Make a Face')
 window.setup(600, 600)  # width and height of the window
 window.bgcolor('gray10')
 
-# Begin!
-t = turtle.Turtle()  # create a Turtle and it always start at (0, 0)
-t.shape("turtle")
-t.speed(0)
+
+def draw_oval():
+    '''
+    t = turtle.Turtle()
+    t.penup()
+    t.goto(0, 0)  # Position the turtle at the center
+    t.speed(0)
+
+    t.fillcolor('gray10')
+    t.begin_fill()
+    t.setheading(-45)
+    for loop in range(2):
+        t.circle(25, 90)
+        t.circle(25/2, 90)
+    t.end_fill()
+    t.hideturtle()
+    '''
+    t = turtle.Turtle()
+    t.penup()
+    t.shape("circle")
+    t.shapesize(2.5, 2.5)
+    t.fillcolor('gray10')
+
+    t.stamp()
+    t.goto(0, 40)
+    t.stamp()
+
+    t.clearstamps()
+    t.hideturtle()
 
 
-t.penup()
-t.goto(100, 100)
-t.left(90)
+def draw_eyes():
+    eyes = turtle.Turtle()
+    eyes.penup()
+    eyes.speed(0)
 
-t.fillcolor('white')
-t.begin_fill()
-t.circle(100, 180)
-t.forward(180)
-t.left(90)
-t.forward(200)
-t.left(90)
-t.forward(180)
-t.end_fill()
+    eyes.fillcolor('gray10')
+    eyes.goto(-40, 80)
+    eyes.begin_fill()
+    eyes.circle(25)
+    eyes.goto(40, 80)
+    eyes.circle(25)
+    eyes.end_fill()
 
-t.goto(100, -80)
-
-
-t.fillcolor('gray10')
-t.begin_fill()
-for _ in range(5):
-    t.circle(20, 180)
-    t.right(180)
-t.end_fill()
+    # eyes.hideturtle()
 
 
-t.hideturtle()
+def draw_body():
+    # Begin!
+    body = turtle.Turtle()  # create a Turtle and it always start at (0, 0)
+    body.shape("turtle")
+    body.speed(0)
 
+    body.penup()
+    body.goto(100, 100)
+    body.left(90)
+
+    body.fillcolor('white')
+    body.begin_fill()
+    body.circle(100, 180)
+    body.forward(180)
+    body.left(90)
+    body.forward(200)
+    body.left(90)
+    body.forward(180)
+    body.end_fill()
+
+    body.goto(100, -80)
+
+    body.fillcolor('gray10')
+    body.begin_fill()
+
+    for _ in range(5):
+        body.circle(20, 180)
+        body.right(180)
+    body.end_fill()
+
+    body.hideturtle()
+
+
+draw_body()
+draw_eyes()
+draw_oval()
 
 window.mainloop()
