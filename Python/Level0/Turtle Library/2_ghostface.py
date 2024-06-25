b9363b84beb16218a4ee8aa53cee7f8b422764cd
jguo108 <chairmanguo@hotmail.com>
2024-06-25 09:36:09 +0800

    update

--- Python/Level0/Turtle Library/2_ghostface.py
+++ Python/Level0/Turtle Library/2_ghostface.py
@@ -11,39 +11,45 @@
 # - reference for costumes:https://www.freepik.com/free-vector/collection-cute-halloween-ghosts_1319212.htm#fromView=search&page=1&position=29&uuid=cf5b65a7-71d0-4fe3-982e-44272b07ac6e
 # - reference for animation: https://scratch.mit.edu/projects/237521692/
 
-# Fullwindow the canvas
-
-window = turtle.Screen()
-window.title('Make a Face')
-window.setup(900, 900)  # width and height of the window
-window.bgcolor('gray10')
-
 face = None
 faces = []
 index = 0
 
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 900
+
 HEAD_RADIUS = 200
 BODY_HEIGHT = 400
 
+NUM_OF_IMAGES = 43
 
-def animate():
+BACKGROUND_COLOR = 'gray10'
+GHOST_COLOR = 'ivory'
+
+
+window = turtle.Screen()
+window.title('Make a Face')
+window.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
+window.bgcolor(BACKGROUND_COLOR)
+
+
+def animate_face():
     global face, index
     index += 1
     face.shape(faces[index % len(faces)])
 
-    window.ontimer(animate, 50)
+    window.ontimer(animate_face, 50)
 
 
 def register_faces():
-    for i in range(43):
+    for i in range(NUM_OF_IMAGES):
         face_gif = f'Resources/make_a_face/{i+1}.gif'
         turtle.register_shape(face_gif)
         faces.append(face_gif)
 
 
 def build_body():
-    global face
-    # Begin!
+   # Begin!
     body = turtle.Turtle()
 
     body.shape("turtle")
@@ -53,7 +59,7 @@ def build_body():
     body.goto(HEAD_RADIUS, 100)
     body.left(90)
 
-    body.fillcolor('white')
+    body.fillcolor(GHOST_COLOR)
     body.begin_fill()
     body.circle(HEAD_RADIUS, 180)
     body.forward(BODY_HEIGHT)
@@ -64,7 +70,7 @@ def build_body():
     body.end_fill()
 
     body.goto(HEAD_RADIUS, 100 - BODY_HEIGHT)
-    body.fillcolor('gray10')
+    body.fillcolor(BACKGROUND_COLOR)
     body.begin_fill()
 
     for _ in range(5):
@@ -73,16 +79,25 @@ def build_body():
     body.end_fill()
     body.hideturtle()
 
+
+def build_face():
+    global face, faces, index
     face = turtle.Turtle()
     face.speed(0)
+    print(index)
+    face.shape(faces[index])
+
     face.penup()
     face.goto(0, 70)
-    face.shape(faces[index])
-    face.shapesize(1, 1)
+
+
+def build_ghost():
+    build_body()
+    build_face()
 
 
 register_faces()
-build_body()
-animate()
+build_ghost()
+animate_face()
 
 window.mainloop()
