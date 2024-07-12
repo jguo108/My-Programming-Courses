a93214c80c7d3b36f1a89fba9f96953d2f323c7e
jguo108 <chairmanguo@hotmail.com>
2024-07-12 18:24:45 +0800

    update

--- Python/Level0/Basics/1 Math Genius/main.py
+++ Python/Level0/Basics/1 Math Genius/main.py
@@ -9,9 +9,14 @@ print("Hello,", name)
 print('Give me two numbers', 'and I can work out the sum, difference,',
       'product and quotient of them!')
 
+# num1_str = input("First number: ")
+# num1 = int(num1_str)
 # num1 = int(input("First number: "))
 num1 = float(input('First number: '))
 # print(num1)
+
+# num2_str = input("Second number: ")
+# num2 = int(num2_str)
 # num2 = int(input("Second number: "))
 num2 = float(input('Second number: '))
 # print(num2)
---
+++ Python/Level0/Basics/2 Animal Quiz/wip.py
@@ -0,0 +1,12 @@
+﻿
+
+print("Welcome to the Animal Quiz!")
+answer = input("Which is the fastest land animal?")
+
+# print(answer)
+# print(answer == "cheetah")
+
+if answer == "cheetah":
+    print("Awesome! You got it right!")
+    print("The first question was pretty easy, huh?")
+    # check_answer(answer=answer1, guess="cheetah")
