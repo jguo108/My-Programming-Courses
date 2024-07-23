b6abd2ae751378cd80936432a30ce7a593a4be7e
jguo108 <chairmanguo@hotmail.com>
2024-07-23 18:34:19 +0800

    update

---
+++ Python/Level0/Basics/4 Pick You Team/wip.py
@@ -0,0 +1,8 @@
+﻿
+
+students = ["Harry", "Alice", "William",
+            "Mary", "James", "Alex", "Sara", "Jenifer"]
+# print(students)
+# print("Students:", ','.join(students))
+for s in students:
+    print(s, end=' ')
