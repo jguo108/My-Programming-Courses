730f140a1f16315b52c08bf5d7ac80aae68317dc
jguo108 <chairmanguo@hotmail.com>
2024-07-25 18:02:34 +0800

    Update wip.py

--- Python/Level0/Basics/4 Pick Your Team/wip.py
+++ Python/Level0/Basics/4 Pick Your Team/wip.py
@@ -1,4 +1,7 @@
-﻿
+﻿# import random
+# from random import randint
+# from random import choice
+from random import *
 
 students = ["Harry", "Alice", "William",
             "Mary", "James", "Alex", "Sara", "Jenifer"]
@@ -18,3 +21,14 @@ print("Students:",)
 
 all_names = ",".join(students)
 print(all_names)
+
+while len(students) != 0:
+    # student_picked = students[random.randint(0, 7)]
+    # student_picked = students[random.randint(0, len(students) - 1)]
+    # student_picked = students[randint(0, len(students) - 1)]
+    student_picked = choice(students)
+    students.remove(student_picked)
+    # print(student_picked)
+    # print(students)
+    team_A.append(student_picked)
+    # print(team_A)
