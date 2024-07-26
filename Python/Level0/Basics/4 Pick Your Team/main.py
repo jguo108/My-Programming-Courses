652474bd8183f11722fddace041b3c0c14b7ef07
jguo108 <chairmanguo@hotmail.com>
2024-07-26 11:32:55 +0800

    update

--- Python/Level0/Basics/4 Pick Your Team/main.py
+++ Python/Level0/Basics/4 Pick Your Team/main.py
@@ -1,64 +1,33 @@
-﻿# Notes:
-# https://www.notion.so/4-Pick-You-Team-4930034ac06f4cbe8b2df35fa6093fcf
+﻿from random import *
 
-from random import choice
-"""
-players = [
-    'Harry', 'John', 'William', 'Jack', 'James', 'Alex', 'Charles', 'Michael',
-    'Daniel'
-]
-"""
 
-players = []
-file = open('./4 Pick Your Team/players.txt', 'r')
-players = file.read().splitlines()
+def print_students(header, students):
+    print(header)
+    print(",".join(students))
 
-# print(players)
-# print(players[0])
-# print(players[1])
-# print(len(players))
+
+# def pick_student(students):
+def pick_student(from_list, to_list):
+    student_picked = choice(from_list)
+    from_list.remove(student_picked)
+    to_list.append(student_picked)
+
+
+students = ["Harry", "Alice", "William",
+            "Mary", "James", "Alex", "Sara", "Jenifer", "Mark"]
 
 team_A = []
 team_B = []
 
+print_students("Students:", students)
 
-def pick_player(players, team):
-    player = choice(players)
-    players.remove(player)
-    if player not in team:
-        team.append(player)
+while len(students) != 0:
+    pick_student(students, team_A)
 
-
-while len(players) != 0:
-    pick_player(players, team_A)
-
-    if players == []:  # Or len(players) == 0
+    if len(students) == 0:
         break
 
-    pick_player(players, team_B)
-"""
-while len(players) != 0:
-    player_A = choice(players)
-    #print(player_A)
-    players.remove(player_A)
+    pick_student(students, team_B)
 
-    if player_A not in team_A:
-        team_A.append(player_A)
-    #print('Players left:', players)
-
-    if players == []:  # Or len(players) == 0
-        break
-
-    player_B = choice(players)
-    #print(player_B)
-    players.remove(player_B)
-
-    if player_B not in team_B:
-        team_B.append(player_B)
-    #print('Players left:', players)
-"""
-
-print('Team A:', ','.join(team_A))
-# print('Team A:', team_A)
-print('Team B:', ','.join(team_B))
-# print('Team B:', team_B)
+print_students("Team A:", team_A)
+print_students("Team B:", team_B)
--- Python/Level0/Basics/4 Pick Your Team/wip.py
+++ Python/Level0/Basics/4 Pick Your Team/wip.py
@@ -3,15 +3,33 @@
 # from random import choice
 from random import *
 
-students = ["Harry", "Alice", "William",
-            "Mary", "James", "Alex", "Sara", "Jenifer"]
+
+def print_students(header, students):
+    print(header)
+    print(",".join(students))
+
+
+# def pick_student(students):
+def pick_student(from_list, to_list):
+    # student_picked = choice(students)
+    student_picked = choice(from_list)
+    from_list.remove(student_picked)
+    to_list.append(student_picked)
+
+
+# students = ["Harry", "Alice", "William",
+#            "Mary", "James", "Alex", "Sara", "Jenifer"]
+
+# students = ["Harry", "Alice", "William",
+#            "Mary", "James", "Alex", "Sara", "Jenifer", "Mark"]
+
+students = []
 
 team_A = []
 team_B = []
 
 
 # print(students)
-print("Students:",)
 
 # all_names = ""
 # for student in students:
@@ -19,9 +37,12 @@ print("Students:",)
 #    all_names += student
 #    all_names += ","
 
-all_names = ",".join(students)
-print(all_names)
+# print("Students:",)
+# all_names = ",".join(students)
+# print(all_names)
+print_students("Students:", students)
 
+'''
 while len(students) != 0:
     # student_picked = students[random.randint(0, 7)]
     # student_picked = students[random.randint(0, len(students) - 1)]
@@ -36,10 +57,22 @@ while len(students) != 0:
     student_picked = choice(students)
     students.remove(student_picked)
     team_B.append(student_picked)
+'''
+
+while len(students) != 0:
+    pick_student(students, team_A)
+
+    if len(students) == 0:
+        break
+
+    pick_student(students, team_B)
 
 
-print("Team A:",)
-print(",".join(team_A))
+# print("Team A:",)
+# print(",".join(team_A))
 
-print("Team B:",)
-print(",".join(team_B))
+# print("Team B:",)
+# print(",".join(team_B))
+
+print_students("Team A:", team_A)
+print_students("Team B:", team_B)
