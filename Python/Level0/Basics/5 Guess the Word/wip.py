07a1d1f3e2ea54fde82bf084d74cde0e787f6558
jguo108 <chairmanguo@hotmail.com>
2024-07-27 09:17:38 +0800

    Update wip.py

--- Python/Level0/Basics/5 Guess the Word/wip.py
+++ Python/Level0/Basics/5 Guess the Word/wip.py
@@ -1,2 +1,8 @@
-﻿
+﻿from random import *
 
+
+words = ["pizza", "teeth", "shirt", "plane", "grass"]
+secret_word = choice(words)
+
+# print("The secret word is:", secret_word)
+print(f"The secrect word is: {secret_word}")
