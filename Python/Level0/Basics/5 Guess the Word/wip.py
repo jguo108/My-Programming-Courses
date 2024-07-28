03878356d29c984ab500587d5915275c890696be
jguo108 <chairmanguo@hotmail.com>
2024-07-28 09:30:18 +0800

    Update wip.py

--- Python/Level0/Basics/5 Guess the Word/wip.py
+++ Python/Level0/Basics/5 Guess the Word/wip.py
@@ -6,3 +6,9 @@ secret_word = choice(words)
 
 # print("The secret word is:", secret_word)
 print(f"The secrect word is: {secret_word}")
+
+# answer = "*****"
+# answer = answer.replace("*", "n", 1)
+# answer[3] = "n"
+answer = ["*", "*", "*", "*", "*"]
+print(answer)
