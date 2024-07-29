fab94fd5bdd529f4fc7b00bb8ac7f2d671523c6c
jguo108 <chairmanguo@hotmail.com>
2024-07-29 18:32:20 +0800

    Update wip.py

--- Python/Level0/Basics/5 Guess the Word/wip.py
+++ Python/Level0/Basics/5 Guess the Word/wip.py
@@ -11,4 +11,20 @@ print(f"The secrect word is: {secret_word}")
 # answer = answer.replace("*", "n", 1)
 # answer[3] = "n"
 answer = ["*", "*", "*", "*", "*"]
-print(answer)
+# print(answer)
+# print(" ".join(answer))
+
+attempts_left = 9
+
+while True:
+    # pass
+    print(" ".join(answer))
+    print(f"Attempts left: {attempts_left}")
+    guess = input("Guess a letter or the whole word: ")
+
+    if len(guess) == 1 secret_word.find(guess):
+        pass
+    # if len(guess) == 1 and secret_word.find(guess)
+
+
+print("End of game")
