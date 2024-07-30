dba5f3ccc10f763772339a9da7454d0259d6151b
jguo108 <chairmanguo@hotmail.com>
2024-07-30 15:14:47 +0800

    Create logical_operators.py

---
+++ Python/Level0/Basics/5 Guess the Word/logical_operators.py
@@ -0,0 +1,34 @@
+﻿
+go_out = False
+
+# 'and' operator
+I_have_finished_howework = False
+friend_has_finished_howework = True
+
+go_out = I_have_finished_howework and friend_has_finished_howework
+print(go_out)
+if go_out:
+    print("We can go out :)")
+else:
+    print("We have to stay home :(")
+
+# 'or' operator
+weekend = True
+holiday = True
+
+go_out = weekend or holiday
+print(go_out)
+if go_out:
+    print("We can go out :)")
+else:
+    print("We have to stay home :(")
+
+# 'not' operator
+raining = True
+
+go_out = not raining
+print(go_out)
+if go_out:
+    print("We can go out :)")
+else:
+    print("We have to stay home :(")
