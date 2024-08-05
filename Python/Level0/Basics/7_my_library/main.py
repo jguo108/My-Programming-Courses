ce3017253cd5746af0402ce6c2d426330469f9e3
jguo108 <chairmanguo@hotmail.com>
2024-08-05 17:05:41 +0800

    update

--- Python/Level0/Basics/7_my_library/main.py
+++ Python/Level0/Basics/7_my_library/main.py
@@ -17,20 +17,20 @@ def print_book(title, info):
     print(f"Title: {title}")
     print(f"  Genre: {info['genre']}")
     print(f"  Year: {info['year']}")
-    print(f"  Main Author: {info['author']}")
+    print(f"  Author: {info['author']}")
 
 
 def add_book():
-    title = input('Enter book title: ')
-    genre = input('Enter book genre: ')
-    year = input('Enter year of release: ')
-    author = input('Enter main author: ')
-    info = {'genre': genre, 'year': year, 'author': author}
+    title = input("Enter book title:")
+    genre = input("Enter book genre:")
+    year = input("Enter year of release:")
+    author = input("Enter author:")
+    info = {"genre": genre, "year": year, "author": author}
     books.update({title: info})
 
 
 def find_books():
-    keyword = input('Enter keyword of book title to find: ')
+    keyword = input("Enter keyword of book title to find:")
 
     matching_titles = []
     for title in books.keys():
@@ -46,23 +46,23 @@ def find_books():
 
 
 def list_books():
-    choice = int(
-        input("Do you want to list books by (1)genre, (2)year, (3)author, (4)all or (5)exit? "))
+    choice = input(
+        "Do you want to list books by (1) genre, (2) year, (3) author, (4) all or (5) exit?")
 
-    if choice == 1:
-        genre = input("Enter genre: ")
-        list_books_by('genre', genre)
-    elif choice == 2:
-        year = input("Enter year: ")
-        list_books_by('year', year)
-    elif choice == 3:
-        author = input("Enter author: ")
-        list_books_by('author', author)
-    elif choice == 4:
+    if choice == "1":
+        genre = input("Enter genre:")
+        list_books_by("genre", genre)
+    elif choice == "2":
+        year = input("Enter year:")
+        list_books_by("year", year)
+    elif choice == "3":
+        author = input("Enter author:")
+        list_books_by("author", author)
+    elif choice == "4":
         print("All books:")
         for title, info in books.items():
             print_book(title, info)
-    elif choice == 5:
+    elif choice == "5":
         return
     else:
         print("Invalid choice.")
@@ -79,14 +79,14 @@ def list_books_by(criteria, value):
 load_books()
 
 while True:
-    choice = int(input("Do you want to (1)add, (2)find, (3)list or (4)exit? "))
-    if choice == 1:
+    choice = input("Do you want to (1) add, (2) find, (3) list or (4) ? ")
+    if choice == "1":
         add_book()
-    elif choice == 2:
+    elif choice == "2":
         find_books()
-    elif choice == 3:
+    elif choice == "3":
         list_books()
-    elif choice == 4:
+    elif choice == "4":
         save_books()
         break
     else:
