3e5024a548ba7671fed30922f13986a62cdff329
jguo108 <chairmanguo@hotmail.com>
2024-08-06 18:32:10 +0800

    Create wip.py

---
+++ Python/Level0/Turtle Library/1_modern_art/wip.py
@@ -0,0 +1,34 @@
+﻿import turtle
+
+
+def setup_window():
+    window.title("Modern Art")
+    window.setup(600, 600)
+    window.bgcolor("gray30")
+
+
+def setup_pen():
+    pen.shape("turtle")
+    pen.color("DarkOrange")
+
+
+window = turtle.Screen()
+pen = turtle.Turtle()
+
+setup_window()
+'''
+window.title("Modern Art")
+window.setup(600, 600)
+# window.bgcolor("red")
+window.bgcolor("gray10")
+'''
+
+setup_pen()
+'''
+pen.shape("turtle")
+pen.color("DarkOrange")
+'''
+
+window.mainloop()
+
+# print("End of program")
