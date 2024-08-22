44cdf306e42ba417a213a92282e4d4446b0b43ad
jguo108 <chairmanguo@hotmail.com>
2024-08-22 13:36:40 +0800

    Update wip.py

--- Python/Level0/Turtle Library/5_dodge_it/wip.py
+++ Python/Level0/Turtle Library/5_dodge_it/wip.py
@@ -7,7 +7,7 @@ import time
 window_width = 600
 window_height = 600
 
-player_speed = 4
+player_speed = 2
 
 num_of_enemy_costumes = 31
 enemy_costumes = []
@@ -20,7 +20,7 @@ def setup_window():
     window.title("Dodge It!")
     window.setup(window_width, window_height)
     window.bgpic("5_dodge_it/Resources/Background/background.gif")
-    # window.tracer(0)
+    window.tracer(0)
 
 
 def setup_player():
@@ -47,6 +47,7 @@ def setup_enemies():
         random.randint(-window_height/2, window_height/2))
     enemy.setheading(random.randint(0, 360))
     '''
+    # for _ in range(10):
     for _ in range(3):
         enemy = turtle.Turtle()
         enemy.shape(enemy_costumes[0])
@@ -68,6 +69,7 @@ def switch_enemy_costume():
     for enemy in enemies:
         enemy.shape(enemy_costumes[i])
     i += 1
+    window.update()
 
     # window.ontimer(switch_jellyfish_costume, 500)
     window.ontimer(switch_enemy_costume, 100)
@@ -130,8 +132,8 @@ def game_loop():
         move_player()
         # move_enemy()
         move_enemies()
-        # window.update()
-        # time.sleep(0.001)
+        window.update()
+        time.sleep(0.01)
 
 
 window = turtle.Screen()
